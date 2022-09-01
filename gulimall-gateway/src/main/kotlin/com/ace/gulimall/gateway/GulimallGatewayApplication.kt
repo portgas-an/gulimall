2c926b26de1fc175f2c1552b32b8bcbe17dd79ba
ace <portgas.an@live.com>
2022-09-01 10:54:38 +0800

    [feat]完成网关

--- gulimall-gateway/src/main/kotlin/com/ace/gulimall/gateway/GulimallGatewayApplication.kt
+++ gulimall-gateway/src/main/kotlin/com/ace/gulimall/gateway/GulimallGatewayApplication.kt
@@ -1,10 +1,12 @@
 package com.ace.gulimall.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-//@EnableDiscoveryClient
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class GulimallGatewayApplication
 
 fun main(args: Array<String>) {
